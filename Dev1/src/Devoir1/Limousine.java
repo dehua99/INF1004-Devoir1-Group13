0ecfadd3130bcfb8a0ec375c70fdd87dbdfa391f
YuntongLong <yuntong.long@uqtr.ca>
2020-06-13 16:30:50 -0400

    creer les class

--- Dev1/src/Devoir1/Limousine.java
+++ Dev1/src/Devoir1/Limousine.java
@@ -1,5 +1,14 @@
 package Devoir1;
 
 public class Limousine {
+	private String num, couleur;
+	private int capacite, nbP;
+	
+	public Limousine(String num, int capacite, int nbP, String chouleur) {
+		this.num= num;
+		this.capacite= capacite;
+		this.nbP= nbP;
+		this.couleur= couleur;
+	}
 
 }
